1f615675fb0291a7f585185f4255140223b4c686
MarcosGorriaran <marcos.gorriaran.7e7@itb.cat>
2024-04-19 12:13:32 +0200

    Created interface DAO

---
+++ Persistance/DAO/IDAO.cs
@@ -0,0 +1,15 @@
+﻿
+
+namespace AC3GUIProgram.Persistance.DAO
+{
+    public interface IDAO
+    {
+        IDAO GetById<T>(T id);
+        public IEnumerable<IDAO> GetAll();
+        public void Add(IDAO entity);
+        public void Add(IEnumerable<IDAO> entities);
+        public void Remove(IDAO entity);
+        public void Remove(IEnumerable<IDAO> entities);
+        public void Update(IDAO entity);
+    }
+}
